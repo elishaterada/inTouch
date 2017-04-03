bae029d338388d7e4dbba215fa966d76f64cc27e
Elisha Terada <elishaterada@gmail.com>
2017-04-02 23:58:52 -0700

    Connect to Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+
+// Firebase Config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCvo5PllHQjwv8w9fboS3HHaeljCvDq6TI',
+  authDomain: 'intouch-6d243.firebaseapp.com',
+  databaseURL: 'https://intouch-6d243.firebaseio.com/',
+  storageBucket: 'gs://intouch-6d243.appspot.com/',
+  messagingSenderId: '738393417478'
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +30,8 @@ import { MainComponent } from './main/main.component';
     HttpModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
