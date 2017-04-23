89ef0dab09c321598ff84d201c071656b11d1cbe
Elisha Terada <elishaterada@gmail.com>
2017-04-23 16:07:11 -0700

    Account for large initials

--- src/app/main/profile-card/profile-card.component.ts
+++ src/app/main/profile-card/profile-card.component.ts
@@ -14,7 +14,7 @@ import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
           <div class="mat-card-avatar mat-card-avatar--lg  profile-card__avatar-wrapper">
             <div class="mat-card-avatar mat-card-avatar--lg  profile-card__avatar"
                  [ngStyle]="{ 'background-image': 'url(' + profile?.avatar + '?d=blank&s=160)' }"></div>
-            <div class="profile-card__initials">
+            <div class="profile-card__initials profile-card__initials--lg">
               {{ profile?.firstName | truncate : '1' }}{{ profile?.lastName | truncate : '1' }}
             </div>
           </div>
